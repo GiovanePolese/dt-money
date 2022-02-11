22a4608610308482553fbac970918f574a04eb58
Giovane Polese <giovanemoraispolese@gmail.com>
2022-02-11 15:42:17 -0300

    feat: added new transaction modal

---
+++ src/components/NewTransactionsModal/index.tsx
@@ -0,0 +1,14 @@
+import Modal from 'react-modal'
+
+interface newTransactionModalProps {
+  isOpen: boolean;
+  onRequestClose: () => void;
+}
+
+export function NewTransactionModal({isOpen, onRequestClose}: newTransactionModalProps) {
+  return (
+    <Modal isOpen={isOpen} onRequestClose={onRequestClose}>
+      <h2>Meu modal</h2>
+    </Modal>
+  );
+}
